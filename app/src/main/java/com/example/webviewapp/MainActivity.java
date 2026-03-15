6cc70a51bda7a03657013b0712fedb5805cc2e07
lawalfataikola123 <borabannergeorge@gmail.com>
2026-03-15 16:33:51 +0100

    Move WebView app to new repository

---
+++ app/src/main/java/com/example/webviewapp/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.webviewapp;
+
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
